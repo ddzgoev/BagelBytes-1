c06a21e1d357bd557ef43f05bfcdd6bacc463625
David Dzgoev <13dzgoevd@gmail.com>
2017-08-30 14:58:49 -0400

    Selecting Utility Spinner results in different payment object properties being sent

--- app/src/main/java/com/bagelbytes/libertyhacksmerge/AddPaymentsActivity.java
+++ app/src/main/java/com/bagelbytes/libertyhacksmerge/AddPaymentsActivity.java
@@ -23,6 +23,7 @@ public class AddPaymentsActivity extends AppCompatActivity implements Serializab
     DBhandler db;
     Payment thePayment;
     PaymentMethod thePaymentMethod;
+    String previousActivitySpinString;
     boolean update = false;
 
     @Override
@@ -32,6 +33,7 @@ public class AddPaymentsActivity extends AppCompatActivity implements Serializab
 
         //Get payment object from previous activity with key "payment" from the intent
         thePayment = (Payment) getIntent().getSerializableExtra("payment");
+        previousActivitySpinString = (String) getIntent().getSerializableExtra("spinSelection").toString();
         if (thePayment.getPay() != null){
             update = true;
         }
@@ -215,9 +217,60 @@ public class AddPaymentsActivity extends AppCompatActivity implements Serializab
         System.out.println("ID: " + id);
         thePayment.setPaymentMethod(id);
         thePayment.setId(0);
-        thePayment.setDate("9/7/2017");
-        thePayment.setPay(240.60);
+        thePayment.setDate("11/7/2017");
+        thePayment.setPay(54.79);
         thePayment.setAuto(1);
+
+        if(previousActivitySpinString.equals("Atlantic City Electric")) {
+                     thePayment.setPaymentMethod(id);
+                     thePayment.setId(0);
+                     thePayment.setDate("9/7/2017");
+            thePayment.setPay(240.60);
+                     thePayment.setAuto(1);
+                 }
+               if(previousActivitySpinString.equals("Consolidated Edison New York")) {
+                        thePayment.setPaymentMethod(id);
+                        thePayment.setId(0);
+                        thePayment.setDate("10/8/2017");
+                        thePayment.setPay(37.87);
+                       thePayment.setAuto(1);
+                    }
+               if(previousActivitySpinString.equals("Eversource Energy")) {
+                        thePayment.setPaymentMethod(id);
+                        thePayment.setId(0);
+                        thePayment.setDate("9/36/2017");
+                        thePayment.setPay(322.31);
+                        thePayment.setAuto(1);
+                    }
+                if(previousActivitySpinString.equals("Hawaii Electric")) {
+                        thePayment.setPaymentMethod(id);
+                    thePayment.setId(0);
+                        thePayment.setDate("9/23/2017");
+                        thePayment.setPay(209.63);
+                        thePayment.setAuto(1);
+                    }
+                if(previousActivitySpinString.equals("Jersey Central Power and Light")) {
+                        thePayment.setPaymentMethod(id);
+                        thePayment.setId(0);
+                        thePayment.setDate("9/7/2017");
+                        thePayment.setPay(177.68);
+                        thePayment.setAuto(1);
+                    }
+                if(previousActivitySpinString.equals("Liberty Mutual Insurance")) {
+                        thePayment.setPaymentMethod(id);
+                        thePayment.setId(0);
+                    thePayment.setDate("11/25/2017");
+                        thePayment.setPay(197.39);
+                        thePayment.setAuto(1);
+                    }
+                if(previousActivitySpinString.equals("Baltimore Gas & Electric")) {
+                        thePayment.setPaymentMethod(id);
+                        thePayment.setId(0);
+                        thePayment.setDate("9/7/2017");
+                        thePayment.setPay(289.49);
+                        thePayment.setAuto(1);
+                    }
+
         //add the payment to the database
         if (update){
             Log.d("TEST:", "UPDATING");
@@ -228,12 +281,7 @@ public class AddPaymentsActivity extends AppCompatActivity implements Serializab
             db.addPayment(thePayment);
         }
 
-//        private Integer id;
-//        private String name;
-//        private  String service; //provider
-//        private  String date;
-//        private  Double pay;
-//        private Integer auto;
+
 
 //display message that payment was succesfully saved
         Toast.makeText(AddPaymentsActivity.this,"Bill Saved Successfully.",Toast.LENGTH_SHORT).show();
